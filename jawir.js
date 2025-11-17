809a5f5f4668dd0a6006b21770683385774387c1
Informatic Smasa Community <iscg15smasa@gmail.com>
2025-11-17 21:49:46 +0700

    Update jawir.js

--- jawir.js
+++ jawir.js
@@ -2,3 +2,19 @@ function toggleMenu() {
   const menu = document.getElementById("contactMenu");
   menu.style.display = menu.style.display === "block" ? "none" : "block";
 }
+
+const floatingBtn = document.querySelector(".floating");
+
+floatingBtn.addEventListener("click", () => {
+    floatingBtn.classList.add("animate");
+
+    setTimeout(() => {
+        floatingBtn.classList.remove("animate");
+    }, 250); // sesuai durasi animasi pop
+});
+
+
+function toggleMenu() {
+  const menu = document.getElementById("contactMenu");
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
